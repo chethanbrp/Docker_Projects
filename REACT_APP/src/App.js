793267afc3a2a76f3b9703f721cdcaa2823ec1e3
chethan <chethanbrkadur@gmail.com>
2026-03-13 07:57:06 +0000

    react application

---
+++ REACT_APP/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{textAlign:"center", marginTop:"100px"}}>
+      <h1>🚀 React Docker Test App</h1>
+      <p>If you see this, Docker deployment works!</p>
+    </div>
+  );
+}
+
+export default App;
